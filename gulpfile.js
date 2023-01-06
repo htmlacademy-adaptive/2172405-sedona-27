e1030eeb82f8f0d6b5b1f17248c4deab7cab84dd
ZollyQuail <zollyquail@gmail.com>
2023-01-06 18:39:22 +0300

    добавляет минификатор

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import less from 'gulp-less';
 import postcss from 'gulp-postcss';
+import csso from 'postcss-csso';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
 
